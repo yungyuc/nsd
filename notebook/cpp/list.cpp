77cd2004474ca9fb5aa915f7517a3643955946d7
Yung-Yu Chen <yyc@solvcon.net>
2019-09-30 17:44:37 +0800

    Add code comments

--- notebook/cpp/list.cpp
+++ notebook/cpp/list.cpp
@@ -5,22 +5,26 @@
 
 int main(int, char **)
 {
+    // Populate lst1.
     std::list<int> lst1;
     for (size_t it=0; it<3; ++it) { lst1.push_back(100+it); }
     std::cout << "lst1:";
     for (int v : lst1) { std::cout << " " << v; }
     std::cout << std::endl;
 
+    // Populate lst2.
     std::list<int> lst2;
     for (size_t it=0; it<3; ++it) { lst2.push_front(200+it); }
     std::cout << "lst2:";
     for (int v : lst2) { std::cout << " " << v; }
     std::cout << std::endl;
 
+    // Use an iterator to search for an element.
     std::list<int>::iterator it;
     for (it=lst2.begin(); it != lst2.end(); ++it)
     { if (*it == 201) { break; } }
 
+    // Erase the found element, and then insert a new one.
     lst2.erase(it++);
     lst2.insert(it, 301);
 
@@ -28,6 +32,7 @@ int main(int, char **)
     for (int v : lst2) { std::cout << " " << v; }
     std::cout << std::endl;
 
+    // Transfer elements from lst1 to lst2 (splice).
     lst2.splice(it, lst1, ++lst1.begin(), lst1.end());
     std::cout << "lst1 (after splice):";
     for (int v : lst1) { std::cout << " " << v; }
