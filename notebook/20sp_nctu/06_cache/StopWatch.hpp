e773d8d5bf71105b52904a887914198bc7e0324c
Yung-Yu Chen <yyc@solvcon.net>
2020-04-07 22:46:29 +0800

    Update stopwatch

--- notebook/20sp_nctu/06_cache/StopWatch.hpp
+++ notebook/20sp_nctu/06_cache/StopWatch.hpp
@@ -1,77 +1,60 @@
-#include <cstdint>
+#pragma once
 
-#if __APPLE__
+#include <chrono>
 
-#include <assert.h>
-#include <CoreServices/CoreServices.h>
-#include <mach/mach.h>
-#include <mach/mach_time.h>
-
-struct StopWatch
+class StopWatch
 {
 
-    StopWatch()
+private:
+
+    using clock_type = std::chrono::high_resolution_clock;
+    using time_type = std::chrono::time_point<clock_type>;
+
+public:
+
+    /// A singleton.
+    static StopWatch & me()
     {
-        mach_timebase_info(&m_tbinfo);
-        lap();
+        static StopWatch instance;
+        return instance;
     }
 
+    StopWatch() : m_start(clock_type::now()), m_stop(m_start) {}
+
+    StopWatch(StopWatch const & ) = default;
+    StopWatch(StopWatch       &&) = default;
+    StopWatch & operator=(StopWatch const & ) = default;
+    StopWatch & operator=(StopWatch       &&) = default;
+    ~StopWatch() = default;
+
+    /**
+     * Return seconds between laps.
+     */
     double lap()
     {
-        m_start = m_end;
-        m_end = mach_absolute_time();
-        uint64_t elapsed = m_end - m_start;
-        return elapsed * m_tbinfo.numer / m_tbinfo.denom * 1.e-9;
+        m_start = m_stop;
+        m_stop = clock_type::now();
+        return std::chrono::duration<double>(m_stop - m_start).count();
     }
 
-    mach_timebase_info_data_t m_tbinfo;
-    uint64_t m_start = 0;
-    uint64_t m_end = 0;
+    /**
+     * Return seconds between end and start.
+     */
+    double duration() const { return std::chrono::duration<double>(m_stop - m_start).count(); }
+
+    /**
+     * Return resolution in second.
+     */
+    static constexpr double resolution()
+    {
+        return double(clock_type::period::num) / double(clock_type::period::den);
+    }
+
+private:
+
+    time_type m_start;
+    time_type m_stop;
 
 }; /* end struct StopWatch */
-
-#elif __linux__
-
-#include <time.h>
-
-struct StopWatch
-{
-    StopWatch()
-    {
-        clock_getres(CLOCK_PROCESS_CPUTIME_ID, &m_res);
-        lap();
-    }
-
-    double lap()
-    {
-        m_start = m_end;
-        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &m_end);
-        return diff(m_start, m_end) * 1.e-9;
-    }
-
-    int64_t diff(timespec const & t1, timespec const & t2)
-    {
-        timespec temp;
-        int64_t nsec;
-        if ((t2.tv_nsec - t1.tv_nsec)<0)
-        {
-            nsec = 1000000000 + t2.tv_nsec - t1.tv_nsec;
-            nsec += 1000000000 * (t2.tv_sec - t1.tv_sec - 1);
-        }
-        else
-        {
-            nsec = t2.tv_nsec - t1.tv_nsec;
-            nsec += 1000000000 * (t2.tv_sec - t1.tv_sec);
-        }
-        return nsec;
-    }
-
-    timespec m_res;
-    timespec m_start;
-    timespec m_end;
-
-}; /* end struct StopWatch */
-
-#endif // __APPLE__
 
 // vim: set ff=unix fenc=utf8 et sw=4 ts=4 sts=4:
