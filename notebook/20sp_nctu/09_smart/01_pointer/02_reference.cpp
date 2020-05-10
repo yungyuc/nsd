3fff1e269f851c7c6e7d82b24329dd06f39058e2
Yung-Yu Chen <yyc@solvcon.net>
2020-05-11 06:18:13 +0800

    Update smart pointer example

--- notebook/20sp_nctu/09_smart/01_pointer/02_reference.cpp
+++ notebook/20sp_nctu/09_smart/01_pointer/02_reference.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <cstdlib>
 
 struct PlainData
@@ -6,40 +7,53 @@ struct PlainData
     int buffer[1024*8];
 }; /* end struct PlainData */
 
+std::ostream & put_ptr(std::ostream & out, void * ptr)
+{
+    out << std::internal << std::setw(18) << std::setfill('0') << ptr;
+    return out;
+}
+
 // The factory function for PlainData.
 PlainData * make_data()
 {
     PlainData * ptr = new PlainData();
-    // In a real factory function, we will do something with the allocated data
-    // object before returning it.
+    // (... work to be done before returning.)
     return ptr;
 }
 
 void manipulate_with_reference(PlainData & data)
 {
-    std::cout << "Manipulate with reference: " << &data << std::endl;
+    std::cout << "Manipulate with reference      : ";
+    put_ptr(std::cout, &data) << std::endl;
 
     for (size_t it=0; it < 1024*8; ++it)
     {
         data.buffer[it] = it;
     }
-    // In a real consumer function we will do much more meaningful operations.
+    // (... more meaningful work before returning.)
 
-    // However, we cannot destruct an object passed in with a reference.
+    // We cannot delete an object passed in with a reference.
 }
 
-int main(int argc, char ** argv)
+int main(int, char **)
 {
     PlainData * ptr = nullptr;
 
+    // Obtain the pointer to the object ('resource').
     ptr = make_data();
-    std::cout << "PlainData pointer after factory: " << ptr << std::endl;
+    std::cout << "PlainData pointer after factory: ";
+    put_ptr(std::cout, ptr) << std::endl;
 
     manipulate_with_reference(*ptr);
 
-    // Destruct the object where we have the pointer.
+    // A good habit when using raw pointer: destruct the object in the scope
+    // that we obtain the pointer.  In this way, we don't forget to delete it
+    // and avoid potential resource leak.
     delete ptr;
-    std::cout << "PlainData pointer after delete: " << ptr << std::endl;
+    std::cout << "PlainData pointer after delete : ";
+    put_ptr(std::cout, ptr) << std::endl;
+
+    return 0;
 }
 
 // vim: set et sw=4 ts=4 sts=4:
